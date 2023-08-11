05645787eb459abf90e759c95e8dc8cab7f382b9
youKeon <yukeon97@gmail.com>
2023-08-11 23:16:56 +0900

    chore : Swagger 세팅

---
+++ backend/src/main/java/com/problem/solving/global/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.problem.solving.global.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// 접속 주소 : http://localhost:8080/swagger-ui/index.html
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("psq")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("PSQ API")
+                        .description("Problem Solving Queue API Docs")
+                        .version("V1"));
+    }
+}
