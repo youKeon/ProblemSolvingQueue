afecb074147071cdf881c0b43dda6468a767e816
youKeon <yukeon97@gmail.com>
2023-08-11 23:52:04 +0900

    refactor : Member API Swagger 적용

--- backend/src/main/java/com/problem/solving/member/presentation/MemberController.java
+++ backend/src/main/java/com/problem/solving/member/presentation/MemberController.java
@@ -4,6 +4,7 @@ import com.problem.solving.member.application.MemberService;
 import com.problem.solving.member.dto.request.MemberSignUpRequest;
 import com.problem.solving.problem.domain.Category;
 import com.problem.solving.problem.dto.response.ProblemListResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +18,14 @@ import java.util.List;
 @RequestMapping("/api/v1/members")
 public class MemberController {
     private final MemberService memberService;
-
+    @Operation(summary = "sign up", description = "회원가입")
     @PostMapping("/signup")
     public ResponseEntity<Void> signup(@RequestBody @Valid MemberSignUpRequest request) {
         memberService.signup(request);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "get problem list", description = "문제 조회")
     @GetMapping("/{id}/problems")
     public ResponseEntity<List<ProblemListResponse>> getProblemList(@PathVariable Long id,
                                                                     @RequestParam(required = false) Integer level,
