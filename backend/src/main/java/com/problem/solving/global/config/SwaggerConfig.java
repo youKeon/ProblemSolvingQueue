0e8b472130ed75999fdbe7a78b65fc754de0aaa4
youKeon <yukeon97@gmail.com>
2023-08-13 01:48:36 +0900

    feat : Swagger에 JSESSIONID 추가

--- backend/src/main/java/com/problem/solving/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/problem/solving/global/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ package com.problem.solving.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,8 +21,14 @@ public class SwaggerConfig {
                 .description("PSQ API");
 
 
+        SecurityScheme auth = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY).in(SecurityScheme.In.COOKIE).name("JSESSIONID");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("AUTH");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes("AUTH", auth))
+                .addSecurityItem(securityRequirement)
                 .info(info);
     }
 }
