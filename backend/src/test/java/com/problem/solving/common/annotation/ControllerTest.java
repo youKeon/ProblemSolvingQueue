dd6face0b16bfdcc395642fafceff75e8f34a663
youKeon <yukeon97@gmail.com>
2023-08-12 16:36:14 +0900

    test : 계층별 어노테이션 수정

--- backend/src/test/java/com/problem/solving/common/annotation/ControllerTest.java
+++ backend/src/test/java/com/problem/solving/common/annotation/ControllerTest.java
@@ -4,14 +4,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.problem.solving.bookmark.application.BookmarkService;
 import com.problem.solving.bookmark.presentation.BookmarkController;
 import com.problem.solving.member.application.MemberService;
+import com.problem.solving.member.domain.Member;
 import com.problem.solving.member.presentation.MemberController;
 import com.problem.solving.problem.application.ProblemService;
 import com.problem.solving.problem.presentation.ProblemController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import javax.servlet.http.HttpServletRequest;
+
 @WebMvcTest({ProblemController.class,
         MemberController.class,
         BookmarkController.class})
@@ -28,4 +35,9 @@ public abstract class ControllerTest {
     protected MemberService memberService;
     @MockBean
     protected BookmarkService bookmarkService;
+
+    protected Member member;
+    protected static Pageable pageable = PageRequest.of(0, 3);
+    protected MockHttpSession session;
+    protected HttpServletRequest request;
 }
--- backend/src/test/java/com/problem/solving/common/annotation/ServiceTest.java
+++ backend/src/test/java/com/problem/solving/common/annotation/ServiceTest.java
@@ -1,19 +1,53 @@
 package com.problem.solving.common.annotation;
 
+import com.problem.solving.bookmark.application.BookmarkService;
+import com.problem.solving.bookmark.domain.Bookmark;
+import com.problem.solving.bookmark.persistence.BookmarkRepository;
+import com.problem.solving.member.application.MemberService;
+import com.problem.solving.member.domain.Member;
+import com.problem.solving.member.domain.SessionInfo;
+import com.problem.solving.member.persistence.MemberRepository;
+import com.problem.solving.problem.application.ProblemService;
 import com.problem.solving.problem.domain.Problem;
 import com.problem.solving.problem.domain.Category;
 import com.problem.solving.problem.persistence.ProblemRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+import javax.servlet.http.HttpServletRequest;
+@ExtendWith(MockitoExtension.class)
 @Transactional
-@ActiveProfiles("test")
 public abstract class ServiceTest {
+    @InjectMocks
+    protected BookmarkService bookmarkService;
+    @InjectMocks
+    protected MemberService memberService;
+    @InjectMocks
+    protected ProblemService problemService;
+    @Mock
+    protected BookmarkRepository bookmarkRepository;
+    @Mock
+    protected ProblemRepository problemRepository;
+    @Mock
+    protected MemberRepository memberRepository;
+    @Mock
+    protected HttpServletRequest request;
 
-    @Autowired protected ProblemRepository problemRepository;
+    protected SessionInfo sessionInfo;
+    protected static Pageable pageable = PageRequest.of(0, 3);
 
+    protected Member member;
+    protected Problem problem1;
+    protected Problem problem2;
+    protected Problem problem3;
+    protected Bookmark bookmark;
 }
