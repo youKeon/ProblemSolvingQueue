2aa57e6c7b1c3fa237c2178c14ec60b0481d4dcd
youKeon <yukeon97@gmail.com>
2023-08-13 01:06:33 +0900

    docs : Swagger 설명 변경

--- backend/src/main/java/com/problem/solving/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/problem/solving/global/config/SwaggerConfig.java
@@ -4,28 +4,23 @@ package com.problem.solving.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 // 접속 주소 : http://localhost:8080/swagger-ui/index.html
 @Configuration
 public class SwaggerConfig {
+
     @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("psq")
-                .pathsToMatch("/api/**")
-                .build();
-    }
-    @Bean
-    public OpenAPI springShopOpenAPI() {
+    public OpenAPI API() {
+        Info info = new Info()
+                .title("Problem Sovling Queue")
+                .version("V1")
+                .description("PSQ API");
+
+
         return new OpenAPI()
-                .info(new Info().title("PSQ API")
-                        .description("Problem Solving Queue API Docs")
-                        .version("V1"));
+                .components(new Components())
+                .info(info);
     }
 }
