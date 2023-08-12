3a7468f051c7539240192f02f929b0ea561e7331
youKeon <yukeon97@gmail.com>
2023-08-12 20:14:05 +0900

    test : 회원가입, 로그인 API 컨트롤러 계층 테스트 코드 작성

--- backend/src/test/java/com/problem/solving/member/presentation/MemberControllerTest.java
+++ backend/src/test/java/com/problem/solving/member/presentation/MemberControllerTest.java
@@ -2,26 +2,15 @@ package com.problem.solving.member.presentation;
 
 import com.problem.solving.common.annotation.ControllerTest;
 import com.problem.solving.member.domain.Member;
-import com.problem.solving.member.domain.SessionInfo;
+import com.problem.solving.member.dto.request.MemberSignInRequest;
 import com.problem.solving.member.dto.request.MemberSignUpRequest;
-import com.problem.solving.member.exception.InvalidMemberException;
-import com.problem.solving.problem.domain.Category;
-import com.problem.solving.problem.dto.response.ProblemListResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.util.ReflectionTestUtils;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.mockito.BDDMockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,16 +21,15 @@ public class MemberControllerTest extends ControllerTest {
     void setup() {
         member = new Member("yukeon97@gmail.com", "123");
         ReflectionTestUtils.setField(member, "id", 1L);
+
+        session = new MockHttpSession();
     }
 
     @Test
     @DisplayName("이메일과 비밀번호를 받아 회원가입을 한다")
-    public void joinMember() throws Exception {
+    public void signUpMember() throws Exception {
         // given
-        MemberSignUpRequest request = new MemberSignUpRequest("email@gmail.com", "1234");
-        willDoNothing()
-                .given(memberService)
-                .signup(request);
+        MemberSignUpRequest request = new MemberSignUpRequest(member.getEmail(), member.getPassword());
 
         // when, then
         mockMvc.perform(post(baseURL + "/signup")
@@ -53,16 +41,12 @@ public class MemberControllerTest extends ControllerTest {
 
     @Test
     @DisplayName("회원가입 시 이메일 형식이 올바르지 않은 경우 예외가 발생한다")
-    public void joinMemberInvalidEmailFormatException() throws Exception {
+    public void signUpMemberInvalidEmailFormatException() throws Exception {
         // given
-        MemberSignUpRequest request = new MemberSignUpRequest("email", "1234");
+        String 잘못된_이메일_형식 = "InvaildEmailFormat";
+        MemberSignUpRequest request = new MemberSignUpRequest(잘못된_이메일_형식, "1234");
 
         // when, then
-        willThrow(new InvalidMemberException("올바른 이메일 형식이어야 합니다."))
-                .given(memberService)
-                .signup(request);
-
-        // then
         mockMvc.perform(post(baseURL + "/signup")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -72,16 +56,11 @@ public class MemberControllerTest extends ControllerTest {
 
     @Test
     @DisplayName("회원가입 시 이메일이 공백인 경우 예외가 발생한다")
-    public void joinMemberInvalidEmailEmptyException() throws Exception {
+    public void signUpMemberInvalidEmailEmptyException() throws Exception {
         // given
         MemberSignUpRequest request = new MemberSignUpRequest("", "1234");
 
-        // when
-        willThrow(new InvalidMemberException("공백일 수 없습니다."))
-                .given(memberService)
-                .signup(request);
-
-        // then
+        // when, then
         mockMvc.perform(post(baseURL + "/signup")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
@@ -91,16 +70,68 @@ public class MemberControllerTest extends ControllerTest {
 
     @Test
     @DisplayName("회원가입 시 비밀번호가 공백인 경우 예외가 발생한다")
-    public void joinMemberInvalidPasswordEmptyException() throws Exception {
+    public void signUpMemberInvalidPasswordEmptyException() throws Exception {
         // given
-        MemberSignUpRequest request = new MemberSignUpRequest("email@gmail.com", "");
+        MemberSignUpRequest request = new MemberSignUpRequest(member.getEmail(), "");
 
-        // when
-        willThrow(new InvalidMemberException("공백일 수 없습니다."))
-                .given(memberService)
-                .signup(request);
+        // when, then
+        mockMvc.perform(post(baseURL + "/signup")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
 
-        // then
+    @Test
+    @DisplayName("이메일과 비밀번호를 받아 로그인을 한다")
+    public void signInMember() throws Exception {
+        // given
+        MemberSignInRequest request = new MemberSignInRequest(member.getEmail(), member.getPassword());
+
+        // when, then
+        mockMvc.perform(post(baseURL + "/signin")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("로그인 시 이메일 형식이 올바르지 않은 경우 예외가 발생한다")
+    public void signInMemberInvalidEmailFormatException() throws Exception {
+        // given
+        String 잘못된_이메일_형식 = "InvaildEmailFormat";
+        MemberSignUpRequest request = new MemberSignUpRequest(잘못된_이메일_형식, "1234");
+
+        // when, then
+        mockMvc.perform(post(baseURL + "/signup")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("로그인 시 이메일이 공백인 경우 예외가 발생한다")
+    public void signInMemberEmptyEmailException() throws Exception {
+        // given
+        MemberSignUpRequest request = new MemberSignUpRequest("", "1234");
+
+        // when, then
+        mockMvc.perform(post(baseURL + "/signup")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("로그인 시 비밀번호가 공백인 경우 예외가 발생한다")
+    public void signInMemberEmptyPasswordException() throws Exception {
+        // given
+        MemberSignUpRequest request = new MemberSignUpRequest(member.getEmail(), "");
+
+        // when, then
         mockMvc.perform(post(baseURL + "/signup")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
