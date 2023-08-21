14a6cdc0c0a878ff32dc5a79cb2e329efb21fbf7
youKeon <yukeon97@gmail.com>
2023-08-22 01:23:59 +0900

    feat : CD 구현

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 BUILD_JAR=$(ls /home/ubuntu/app/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/app/deploy.log
 
 echo "> build 파일 복사" >> /home/ubuntu/app/deploy.log
-DEPLOY_PATH=/home/ubuntu/app/action/
+DEPLOY_PATH=/home/ubuntu/app/
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/app/deploy.log
