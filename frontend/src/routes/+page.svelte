b54fd27428009de933eaf56cf7575bee3e9f486b
TMInstaller <ajvldmlqjqclr@naver.com>
2023-09-02 22:21:53 +0900

    페이지 추가 설명 작성

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,3 @@
+<!-- SvelteKit에서 src/routes 하위에 있는 
+  모든 +page.svelte 페이지는 index 파일의 역할을 합니다 -->
 <h1>시작 페이지</h1>
--- frontend/src/routes/problem/+page.svelte
+++ frontend/src/routes/problem/+page.svelte
@@ -1,2 +1,22 @@
 <!-- 여기는 문제 리스트 페이지입니다. -->
+<script lang="ts">
+  import { getProblemsList } from "../../api/api";
+
+  async function fetchProblems(){
+    const pageableData = {
+      page: 0,
+      size: 10,
+      sort: ["level,"]
+    };
+
+    try{
+      const problems = await getProblemsList(pageableData);
+      console.log('Fetched problems: ', problems);
+    } catch (error){
+      console.log("문제 목록 조회 중 오류 발생:", error);
+    }
+  }
+
+  fetchProblems();
+</script>
 <h1>여기는 problems 페이지입니다.</h1>
