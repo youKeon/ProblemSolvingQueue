098029c933dcb442fb16b4b4a48e910e18accd00
TMInstaller <ajvldmlqjqclr@naver.com>
2023-09-05 18:50:09 +0900

    개발중 페이지 연결

---
+++ frontend/src/routes/member/signin/+page.svelte
@@ -0,0 +1,20 @@
+<div class="login-container">
+  <div class="logo"></div>
+  <h1>로그인하세요</h1>
+  <p>풀었던 문제를 관리해보세요</p>
+  <input type="email" placeholder="이메일">
+  <input type="password" placeholder="비밀번호">
+  <a href="/special/under-construction">비밀번호를 잊으셨나요?</a>
+  <button>로그인</button>
+  <div class="footer-text">
+      <span>Problem Solving Queue가 처음이세요?</span> 
+        <a href="/special/under-construction">회원가입</a>      
+  </div>
+</div>
+
+<style>
+  .logo{
+
+  }
+  
+</style>
---
+++ frontend/src/routes/special/under-construction/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  function goBack() {
+    window.history.back();
+  }
+</script>
+
+<h1>현재 개발중입니다</h1>
+<button on:click={goBack}>Go Back</button>
